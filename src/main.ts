91c44c6912e18b30dd7c5e70a31f1947635065f8
widse <wngud6012@naver.com>
2025-01-11 22:51:05 +0900

    fix: swagger main description

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Stocky Backend')
+    .setDescription('Stocky Backend API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
